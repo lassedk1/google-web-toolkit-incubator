43abd8a08e173dd962f95ad106f5a1b825d14524
gwt.team.ecc <gwt.team.ecc@6497d715-d039-0410-8d7d-ef101778ce2c>
2007-10-15 17:57:00 +0000

    Updated PinnedPanel

--- trunk/src/com/google/gwt/widgetideas/client/PinnedPanel.java
+++ trunk/src/com/google/gwt/widgetideas/client/PinnedPanel.java
@@ -30,9 +30,49 @@ import com.google.gwt.user.client.ui.Widget;
 /**
  * {@link PinnedPanel} creates a panel that is, by default, pinned in place.
  * When the pinned state it toggled, the contents of the panel will display only
- * when the users mouse hovers over it.
+ * when the users mouse hovers over it, otherwise is will collapse to the left.
+ * <p>
+ * The default style name is gwt-PinnedPanel.
+ * <p>
+ * Planned enhancements: Allow panel to be collapsed in arbitrary direction.
  */
 public class PinnedPanel extends Composite {
+  DelayAction delayAction = new DelayAction();
+
+  private class DelayAction extends Timer {
+    private static final int DELAY_MILLI = 20;
+    private static final int NONE = -10000;
+    int savedState;
+
+    public void activate() {
+      if (savedState == state.currentState) {
+        // Do nothing, timer is already running.
+        return;
+      } else if (savedState != NONE) {
+        this.cancel();
+      }
+      savedState = state.currentState;
+      delayAction.schedule(DELAY_MILLI);
+    }
+
+    public void run() {
+      if (savedState != state.currentState) {
+        throw new IllegalStateException("How did this happen?" + savedState
+            + " current state:" + state.currentState);
+      }
+      if (state.currentState == State.WILL_HIDE) {
+        impl.hide();
+      } else if (state.currentState == State.WILL_SHOW) {
+        impl.show();
+      } else {
+        throw new IllegalStateException("Why are we in state "
+            + state.currentState + " rather than state " + savedState);
+      }
+
+      savedState = NONE;
+    }
+  }
+
   /**
    * Pinned panel impl.
    */
@@ -102,25 +142,27 @@ public class PinnedPanel extends Composite {
     }
 
     protected void hide() {
+      pin.state.currentState = State.HIDING;
       overlayTimer.cancel = true;
       hidingTimer.cancel = false;
-      hidingTimer.interval = (pin.width - pin.peekoutSize)
-          / NUMBER_OF_INTERVALS;
+      hidingTimer.interval =
+          (pin.width - pin.peekoutSize) / NUMBER_OF_INTERVALS;
       hidingTimer.run();
     }
 
     protected abstract void setPanelWidth(int width);
 
     protected void show() {
+      pin.state.currentState = State.SHOWING;
       overlayTimer.cancel = false;
       hidingTimer.cancel = true;
-      overlayTimer.interval = (pin.width - pin.peekoutSize)
-          / NUMBER_OF_INTERVALS;
+      overlayTimer.interval =
+          (pin.width - pin.peekoutSize) / NUMBER_OF_INTERVALS;
       overlayTimer.run();
     }
 
     protected void showPinned() {
-      DOM.setStyleAttribute(e, "marginRight", pin.getRightMargin() + "px");
+      // for later cleanup code.
     }
   }
 
@@ -137,10 +179,12 @@ public class PinnedPanel extends Composite {
 
   static class State {
     static int MUST_HIDE = 0;
-    static int HIDING = 1;
-    static int IS_HIDDEN = 2;
-    static int SHOWING = -1;
-    static int IS_SHOWN = -2;
+    static int WILL_HIDE = 1;
+    static int HIDING = 2;
+    static int IS_HIDDEN = 3;
+    static int WILL_SHOW = -1;
+    static int SHOWING = -2;
+    static int IS_SHOWN = -3;
     int currentState;
 
     public boolean shouldHide() {
@@ -169,11 +213,11 @@ public class PinnedPanel extends Composite {
   private ToggleButton switchButton;
 
   /**
-   * Create a new Pinned Pane. The stickout toggle button is always going to be
-   * displayed. It must have a fixed width.
+   * Create a new {@link PinnedPanel}. The toggle button is always going to be
+   * displayed.
    * 
-   * @param width width
-   * @param pinnedToggle the button to toggle the collapsing state.
+   * @param width width of the panel in it's exposed state.
+   * @param pinnedToggle the button to toggle the collapsing state
    */
   public PinnedPanel(int width, ToggleButton pinnedToggle, Widget contents) {
     this.switchButton = pinnedToggle;
@@ -200,13 +244,14 @@ public class PinnedPanel extends Composite {
           }
         }
         super.onBrowserEvent(event);
+        setStyleName(DEFAULT_STYLENAME);
       }
 
       private void maybeHide(Element e) {
         if (!DOM.isOrHasChild(this.getElement(), e)) {
           if ((state.shouldHide())) {
-            state.currentState = State.HIDING;
-            impl.hide();
+            state.currentState = State.WILL_HIDE;
+            delayAction.activate();
           }
         }
       }
@@ -214,20 +259,21 @@ public class PinnedPanel extends Composite {
       private void maybeShow(Element e) {
         if (!DOM.isOrHasChild(this.getElement(), e)) {
           if ((state.shouldShow())) {
-            state.currentState = State.SHOWING;
-            impl.show();
+            state.currentState = State.WILL_SHOW;
+            delayAction.activate();
           }
         }
       }
 
     };
-
+    container.setSize("100%", "100%");
     initWidget(container);
     innerContents.setHeight("100%");
     innerContents.add(contents);
+    innerContents.setCellWidth(contents, "100%");
+    innerContents.setCellHeight(contents, "100%");
     innerContents.add(switchButton);
     innerContents.setCellHeight(switchButton, "100%");
-    innerContents.setCellWidth(switchButton, "1px");
 
     container.setWidget(innerContents);
     impl.setPinnedPanel(this);
@@ -258,7 +304,7 @@ public class PinnedPanel extends Composite {
    * As {@link PinnedPanel} needs to manipulate the right margin, the right
    * margin must be controlled in code.
    * 
-   * @param rightMargin
+   * @param rightMargin in px
    */
   public void setRightMargin(int rightMargin) {
     this.userDefinedRightMargin = rightMargin;
@@ -277,10 +323,11 @@ public class PinnedPanel extends Composite {
    * browser's document.
    */
   protected void onLoad() {
-    int clientWidth = DOM.getElementPropertyInt(this.getElement(),
-        "clientWidth");
+    int clientWidth =
+        DOM.getElementPropertyInt(this.getElement(), "clientWidth");
     innerContents.setWidth(clientWidth + "px");
     this.peekoutSize = switchButton.getOffsetWidth();
+    innerContents.setCellWidth(switchButton, peekoutSize + "px");
   }
 
   private void setPinned(boolean pinned) {
