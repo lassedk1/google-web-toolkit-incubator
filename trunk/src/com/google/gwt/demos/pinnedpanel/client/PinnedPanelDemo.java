a25c389f053a482940277bdd7df4614bfb986ce6
gwt.team.ecc <gwt.team.ecc@6497d715-d039-0410-8d7d-ef101778ce2c>
2007-10-17 19:39:51 +0000

    Adding pinned panel and bulk loading table demos

--- trunk/src/com/google/gwt/demos/pinnedpanel/client/PinnedPanelDemo.java
+++ trunk/src/com/google/gwt/demos/pinnedpanel/client/PinnedPanelDemo.java
@@ -1,17 +1,29 @@
+/*
+ * Copyright 2007 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.google.gwt.demos.pinnedpanel.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.ToggleButton;
 import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.widgetideas.client.PinnedPanel;
 
@@ -19,7 +31,6 @@ import com.google.gwt.widgetideas.client.PinnedPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class PinnedPanelDemo implements EntryPoint {
-
 
   /**
    * This is the entry point method.
@@ -39,22 +50,12 @@ public class PinnedPanelDemo implements EntryPoint {
     pinned.setRightMargin(3);
 
     RootPanel.get("pinned-panel").add(pinned);
-
-
-
-  }
-
-  private ToggleButton createToggleButton() {
-    Image show = new Image("show.jpg");
-    Image hide = new Image("hide.jpg");
-    ToggleButton toggler = new ToggleButton(hide, show);
-    return toggler;
   }
 
   private Widget createSchoolNavBar() {
     FlowPanel navBar = new FlowPanel();
     navBar.setSize("100%", "100%");
-    
+
     HTML title = new HTML("School Directory");
     title.setStyleName("nav-tree-title");
     navBar.add(title);
@@ -72,5 +73,12 @@ public class PinnedPanelDemo implements EntryPoint {
     TreeItem admin = contents.addItem("Administrators");
     admin.addItem("The Soup Nazi");
     return navBar;
+  }
+
+  private ToggleButton createToggleButton() {
+    Image show = new Image("show.jpg");
+    Image hide = new Image("hide.jpg");
+    ToggleButton toggler = new ToggleButton(hide, show);
+    return toggler;
   }
 }
