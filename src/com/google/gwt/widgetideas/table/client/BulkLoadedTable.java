fdba1d2138fd464cde2843bf813e0e5b035c0a02
gwt.team.ecc <gwt.team.ecc@6497d715-d039-0410-8d7d-ef101778ce2c>
2007-10-19 21:57:37 +0000

    Changed docs to doc to be consistent with GWT, also changed wiki to wiki-support in order to avoid confusion with top-level wiki dir.

--- src/com/google/gwt/widgetideas/table/client/BulkLoadedTable.java
+++ src/com/google/gwt/widgetideas/table/client/BulkLoadedTable.java
@@ -16,36 +16,28 @@
 
 package com.google.gwt.widgetideas.table.client;
 
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.widgetideas.table.client.overrides.FlexTable;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
  * Table data, provided by a specific{@link TableModel}, is bulk loaded into
  * the current table.
  */
-public class BulkLoadedTable extends FlexTable {
+public class BulkLoadedTable extends FlexTable implements
+    BulkLoadableImpl.BulkLoadable {
+
+  private BulkLoadableImpl impl;
 
   /**
-   * Controls how each cell is rendered. By default, calls toString on the cell
-   * data.
+   * Constructor.
    */
-  public static class Renderer {
-    public String renderCell(int row, int cell, Object cellData) {
-      return cellData.toString();
-    }
+  public BulkLoadedTable() {
+    impl = new BulkLoadableImpl(this);
   }
 
-  /**
-   * Scratch Element used to load table and row strings.
-   */
-  private static final Element WRAPPER_DIV = DOM.createElement("div");
-
-  private Renderer renderer = new Renderer();
+  private StringCellRenderer renderer = new StringCellRenderer();
 
   /**
    * Sets the table's rows to the list of rows provided.
@@ -53,8 +45,7 @@ public class BulkLoadedTable extends FlexTable {
    * @param rows list of rows. Each row itself must be another list
    */
   public void renderRows(List rows) {
-    TableModel.ListTableModel tableModel = new TableModel.ListTableModel(rows);
-    renderRows(tableModel, 0, rows.size());
+    impl.renderRows(rows);
   }
 
   /**
@@ -64,6 +55,11 @@ public class BulkLoadedTable extends FlexTable {
    */
   public void renderRows(TableModel tableModel) {
     renderRows(tableModel, 0, TableModel.ALL_ROWS);
+  }
+
+  /** Sets the body element of the table. */
+  public void setBodyElement(Element e) {
+    super.setBodyElement(e);
   }
 
   /**
@@ -76,29 +72,7 @@ public class BulkLoadedTable extends FlexTable {
    *          indicates all of them
    */
   public void renderRows(TableModel tableModel, int startRow, int numRows) {
-    // TODO clear all widgets in widget cache.
-    tableModel.requestRows(new TableModel.Request(startRow, numRows),
-        new TableModel.Callback() {
-          public void onRowsReady(TableModel.Request request,
-              TableModel.Response response) {
-            JavaScriptObject temp = tempNew();
-            int index = 0;
-            tempAdd(temp, "<table><tbody>");
-            Iterator rows = response.getRows();
-            for (int rowIndex = 0; rows.hasNext(); ++rowIndex) {
-              tempAdd(temp, "<tr>");
-              Iterator row = (Iterator) rows.next();
-              for (int cellIndex = 0; row.hasNext(); ++cellIndex) {
-                tempAdd(temp, "<td>");
-                tempAdd(temp, renderCell(rowIndex, cellIndex, row.next()));
-                tempAdd(temp, "</td>");
-              }
-              tempAdd(temp, "</tr>");
-            }
-            tempAdd(temp, "</tbody> </table>");
-            renderRows(toStringTemp(temp));
-          }
-        });
+    impl.renderRows(tableModel, startRow, numRows);
   }
 
   /**
@@ -107,52 +81,11 @@ public class BulkLoadedTable extends FlexTable {
    * 
    * @param renderer the renderer
    */
-  public void setRenderer(Renderer renderer) {
+  public void setRenderer(StringCellRenderer renderer) {
     this.renderer = renderer;
   }
 
-  /**
-   * Renders the given cell. Replace the {@link Renderer} to change how the
-   * rendering is performed.
-   * 
-   * @param row row
-   * @param cell cell
-   * @param cellData cellData used to render cell
-   * @return the rendered cell
-   */
-  protected final String renderCell(int row, int cell, Object cellData) {
-    return renderer.renderCell(row, cell, cellData);
+  public StringCellRenderer getRenderer() {
+    return renderer;
   }
-
-  private void renderRows(String rawHTMLTable) {
-    // TODO when assertions are added, extra checks should be added to check
-    // that the raw html is valid for this table.
-    DOM.setInnerHTML(WRAPPER_DIV, rawHTMLTable);
-    Element table = DOM.getFirstChild(WRAPPER_DIV);
-    Element newBody = renderRowsImpl(this.getElement(), table);
-    setBodyElement(newBody);
-  }
-
-  /**
-   * Helper native method for <code>initRows</code>.
-   */
-  private native Element renderRowsImpl(Element table, Element thatBody) /*-{ 
-    table.removeChild(table.tBodies[0]);
-    var thatChild = thatBody.tBodies[0];
-    table.appendChild(thatChild);
-    return thatChild;
-  }-*/;
-
-  private native void tempAdd(JavaScriptObject temp, String stuff) /*-{
-    temp[temp.length] = stuff;
-  }-*/;
-
-  private native JavaScriptObject tempNew() /*-{
-    return [];
-  }-*/;
-
-  private native String toStringTemp(JavaScriptObject temp) /*-{ 
-    return temp.join("");
-  }-*/;
-
 }
