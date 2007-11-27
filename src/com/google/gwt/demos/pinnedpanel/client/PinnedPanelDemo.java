0d40874b82b736c91860406d9254de9f1e809139
gwt.team.ecc <gwt.team.ecc@6497d715-d039-0410-8d7d-ef101778ce2c>
2007-11-27 16:20:36 +0000

    PinnedPanel upgrades

--- src/com/google/gwt/demos/pinnedpanel/client/PinnedPanelDemo.java
+++ src/com/google/gwt/demos/pinnedpanel/client/PinnedPanelDemo.java
@@ -17,23 +17,69 @@
 package com.google.gwt.demos.pinnedpanel.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.ChangeListener;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.WindowResizeListener;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.StackPanel;
 import com.google.gwt.user.client.ui.ToggleButton;
 import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.widgetideas.client.PinnedPanel;
 
+import java.util.ArrayList;
+
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class PinnedPanelDemo implements EntryPoint {
+
+  /**
+   * A special purpose widget to allow scrollable stack panels.
+   */
+  public class MyStackPanel extends StackPanel {
+    private ArrayList scrollers = new ArrayList();
+
+    public void insert(Widget w, int before) {
+      ScrollPanel p = new ScrollPanel(w);
+      p.setWidth("100%");
+      scrollers.add(before, p);
+      super.insert(p, before);
+    }
+
+    public void onLoad() {
+      setWidth("100%");
+      showStack(getSelectedIndex());
+    }
+
+    /**
+     * Shows the widget at the specified child index.
+     * 
+     * @param index the index of the child to be shown
+     */
+    public void showStack(int index) {
+      super.showStack(index);
+
+      if (this.isAttached()) {
+        ScrollPanel me = (ScrollPanel) scrollers.get(index);
+        me.setHeight("1px");
+        Element tr = DOM.getChild(DOM.getFirstChild(getElement()),
+            index * 2 + 1);
+        int trHeight = DOM.getElementPropertyInt(tr, "offsetHeight");
+        me.setHeight(trHeight + "px");
+      }
+    }
+  }
+
+  MyStackPanel wrapper;
 
   /**
    * This is the entry point method.
@@ -45,27 +91,25 @@ public class PinnedPanelDemo implements EntryPoint {
 
     // Some random contents to make the tree interesting.
     Widget contents = createSchoolNavBar(toggler);
- 
 
-    // The actual pinned panel. 
+    // The actual pinned panel.
     final PinnedPanel pinned = new PinnedPanel(200, toggler, contents);
 
     // Right margin needs to be set programmatically as it is used to create the
     // panel slide-out effect.
     pinned.setRightMargin(10);
 
-    // Add a change listener
-    pinned.addChangeListener(new ChangeListener() {
-
-      public void onChange(Widget sender) {
-        RootPanel.get().add(new Label("Pinned: " + pinned.isPinned()));
-      }
-    });
-
-     
     RootPanel.get("pinned-panel").add(pinned);
-  }
 
+    sizePinnedPanel(pinned, Window.getClientHeight());
+    Window.addWindowResizeListener(new WindowResizeListener() {
+      public void onWindowResized(int width, int height) {
+
+        sizePinnedPanel(pinned, height);
+      }
+
+    });
+  }
 
   private Widget createSchoolNavBar(ToggleButton toggler) {
     FlowPanel navBar = new FlowPanel();
@@ -78,12 +122,19 @@ public class PinnedPanelDemo implements EntryPoint {
     navBar.add(panel);
 
     panel.setStyleName("nav-tree-title");
+    wrapper = new MyStackPanel();
+    wrapper.setHeight("250px");
     Tree contents = new Tree();
-    navBar.add(contents);
+    wrapper.add(contents, "<b>People</b>", true);
+
+    wrapper.add(new Label("None"), "<b>Academics</b>", true);
+    navBar.add(wrapper);
 
     TreeItem students = contents.addItem("Students");
     students.addItem("Jill");
     students.addItem("Jack");
+    students.addItem("Molly");
+    students.addItem("Ms. Muffat");
 
     TreeItem teachers = contents.addItem("Teachers");
     teachers.addItem("Mrs Black");
@@ -100,5 +151,12 @@ public class PinnedPanelDemo implements EntryPoint {
     Image hide = new Image("hide.jpg");
     ToggleButton toggler = new ToggleButton(hide, show);
     return toggler;
+  }
+
+  private void sizePinnedPanel(final PinnedPanel pinned, int height) {
+    // TODO(ECC) remove magic numbers when I have time to pound in it.
+    pinned.setHeight(height - 120 + "px");
+    wrapper.setHeight(height - 160 + "px");
+    wrapper.showStack(wrapper.getSelectedIndex());
   }
 }
