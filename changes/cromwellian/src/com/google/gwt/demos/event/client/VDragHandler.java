cc6dde11afba3a043208d6bef3c995c4848fc2f0
cromwellian <cromwellian@6497d715-d039-0410-8d7d-ef101778ce2c>
2008-07-21 23:41:42 +0000

    Inprogress virtual event demo on GWTCanvas

---
+++ changes/cromwellian/src/com/google/gwt/demos/event/client/VDragHandler.java
@@ -0,0 +1,7 @@
+package com.google.gwt.demos.event.client;
+
+import com.google.gwt.libideas.event.shared.EventHandler;
+
+public interface VDragHandler extends EventHandler {
+  void onDrag(VDragEvent event);
+}
