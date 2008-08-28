6efd898f5d022edb4a9d63d46fdd1e76e4373d56
ecc <ecc@google.com>
2008-08-28 21:55:56 +0000

    To try and reduce the complexity of the ongoing custom list box review and to avoid making Ray's like heck by locking  the event sub-system, committing the gen2 infrastructure.
    As subversion is complaining, doing so in stages.

---
+++ src/com/google/gwt/gen2/event/dom/client/ChangeEvent.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.libideas.event.client;
+
+import com.google.gwt.libideas.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Event;
+
+/**
+ * Represents a ChangeEvent event.
+ */
+public class ChangeEvent extends BrowserEvent<ChangeHandler> {
+
+  /**
+   * A widget that implements this interface is a public source of ChangeEvent
+   * events.
+   */
+  public static interface Source {
+
+    /**
+     * Adds a {@link ChangeEvent} handler.
+     *
+     * @param handler the handler
+     */
+    HandlerRegistration addChangeHandler(ChangeHandler handler);
+  }
+
+  public static Key<ChangeHandler> KEY = new Key<ChangeHandler>(
+      BrowserEvents.ONCHANGE);
+
+  public ChangeEvent(Event e) {
+    super(KEY, e);
+  }
+
+  /**
+   * Fires a ChangeEvent.
+   *
+   * @param handler the handler
+   */
+  protected void fireEvent(ChangeHandler handler) {
+    handler.onChange(this);
+  }
+}
