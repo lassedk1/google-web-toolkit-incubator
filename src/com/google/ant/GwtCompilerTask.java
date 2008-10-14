5720146fef4bfd2c7da1249e5c702159a2bc9e21
itruett <itruett@6497d715-d039-0410-8d7d-ef101778ce2c>
2008-10-14 20:33:16 +0000

    Added support for setting JS output style, VM max memory, and log level. Added support for nested <classpath>.

--- src/com/google/ant/GwtCompilerTask.java
+++ src/com/google/ant/GwtCompilerTask.java
@@ -15,26 +15,35 @@
  */
 package com.google.ant;
 
-import com.google.gwt.core.ext.UnableToCompleteException;
-import com.google.gwt.core.ext.TreeLogger.Type;
 import com.google.gwt.dev.GWTCompiler;
-import com.google.gwt.dev.cfg.ModuleDef;
-import com.google.gwt.dev.cfg.ModuleDefLoader;
-import com.google.gwt.dev.util.log.PrintWriterTreeLogger;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.Execute;
+import org.apache.tools.ant.taskdefs.Redirector;
+import org.apache.tools.ant.types.CommandlineJava;
+import org.apache.tools.ant.types.Path;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Compiles a GWT module.
  */
 public class GwtCompilerTask extends Task {
-  private String src = "src";
-  private String out = "www";
+  private CommandlineJava command = new CommandlineJava();
+  private String logLevel = "WARN";
   private String moduleFile;
   private String moduleName;
+  private String out = "www";
+  private String src = "src";
+  private String style = "PRETTY";
+  private String vmMaxMemory = "512m";
+
+  public Path createClasspath() {
+    return command.createClasspath(getProject());
+  }
 
   /**
    * @see org.apache.tools.ant.Task#execute()
@@ -44,42 +53,43 @@ public class GwtCompilerTask extends Task {
     final File srcDir = new File(src);
 
     if (moduleName == null) {
-      moduleName = convertModuleFileToName(srcDir, moduleFile);
+      if (moduleFile == null) {
+        throw new BuildException("Required: either moduleName or moduleFile");
+      } else {
+        if (new File(moduleFile).exists()) {
+          moduleName = convertModuleFileToName(srcDir, moduleFile);
+        } else {
+          throw new BuildException("File not found: " + moduleFile);
+        }
+      }
     }
 
-    System.out.println("Compiling " + moduleName);
+    command.setClassname(GWTCompiler.class.getCanonicalName());
+    command.createArgument().setLine("-logLevel " + logLevel);
+    command.createArgument().setLine("-out " + out);
+    command.createArgument().setLine("-style " + style);
+    command.createArgument().setValue(moduleName);
+    command.createVmArgument().setValue("-Xmx" + vmMaxMemory);
 
-    final GWTCompiler compiler = new GWTCompiler();
+    log("Compiling " + moduleName, Project.MSG_INFO);
 
-    compiler.setModuleName(moduleName);
-    compiler.setLogLevel(Type.ERROR);
-    compiler.setOutDir(new File(out));
-    compiler.setStylePretty();
-
-    final PrintWriterTreeLogger logger = new PrintWriterTreeLogger();
-    logger.setMaxDetail(compiler.getLogLevel());
+    final Redirector redirector = new Redirector(this);
+    final Execute exe = new Execute(redirector.createHandler(), null);
+    exe.setAntRun(getProject());
+    exe.setCommandline(command.getCommandline());
 
     try {
-      final ModuleDef moduleDef = ModuleDefLoader.loadFromClassPath(logger,
-          moduleName);
-      compiler.distill(logger, moduleDef);
-    } catch (UnableToCompleteException e) {
+      exe.setNewenvironment(true);
+      exe.setVMLauncher(false);
+      exe.execute();
+      redirector.complete();
+    } catch (IOException e) {
       throw new BuildException(e);
     }
   }
 
-  private String convertModuleFileToName(final File srcDir,
-      final String moduleFile) {
-    return moduleFile.replace(File.separatorChar, '.').substring(
-        srcDir.getAbsolutePath().length() + 1, moduleFile.length() - 8);
-  }
-
-  public void setSrc(final String src) {
-    this.src = src;
-  }
-
-  public void setOut(final String out) {
-    this.out = out;
+  public void setLogLevel(final String logLevel) {
+    this.logLevel = logLevel.toUpperCase();
   }
 
   public void setModuleFile(final String moduleFile) {
@@ -88,5 +98,30 @@ public class GwtCompilerTask extends Task {
 
   public void setModuleName(final String moduleName) {
     this.moduleName = moduleName;
+  }
+
+  public void setOut(final String out) {
+    this.out = out;
+  }
+
+  public void setSrc(final String src) {
+    this.src = src;
+  }
+
+  public void setStyle(final String style) {
+    this.style = style.toUpperCase();
+  }
+
+  public void setVmMaxMemory(final String vmMaxMemory) {
+    this.vmMaxMemory = vmMaxMemory;
+  }
+
+  private String convertModuleFileToName(final File srcDir,
+      final String moduleFile) {
+    log("Converting file name " + moduleFile
+        + " to a module name relative to source root "
+        + srcDir.getAbsolutePath(), Project.MSG_DEBUG);
+    return moduleFile.replace(File.separatorChar, '.').substring(
+        srcDir.getAbsolutePath().length() + 1, moduleFile.length() - 8);
   }
 }
