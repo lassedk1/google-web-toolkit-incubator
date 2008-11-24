f3c20f96bc71bc819228022977ef2b4372b1e7c1
daniel.florey <daniel.florey@6497d715-d039-0410-8d7d-ef101778ce2c>
2008-11-24 10:41:54 +0000

    Added TreeTable demo

--- branches/dflorey/tableAddons/src-demo/com/google/gwt/gen2/demo/scrolltable/client/TreeTableDemo.java
+++ branches/dflorey/tableAddons/src-demo/com/google/gwt/gen2/demo/scrolltable/client/TreeTableDemo.java
@@ -2,27 +2,16 @@ package com.google.gwt.gen2.demo.scrolltable.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.gen2.datepicker.client.DatePicker;
-import com.google.gwt.gen2.demo.scrolltable.client.TreeTableDemo.ClientFileTableModel.TreeItemComparator;
-import com.google.gwt.gen2.table.client.ColumnDefinition;
 import com.google.gwt.gen2.table.client.DateColumnDefinition;
 import com.google.gwt.gen2.table.client.DefaultTableDefinition;
-import com.google.gwt.gen2.table.client.FixedWidthFlexTable;
-import com.google.gwt.gen2.table.client.FixedWidthGrid;
 import com.google.gwt.gen2.table.client.NumberColumnDefinition;
-import com.google.gwt.gen2.table.client.PagingOptions;
-import com.google.gwt.gen2.table.client.PagingScrollTreeTable;
 import com.google.gwt.gen2.table.client.ScrollTable;
 import com.google.gwt.gen2.table.client.TextColumnDefinition;
-import com.google.gwt.gen2.table.client.TreeTableModel;
+import com.google.gwt.gen2.table.client.TreeItem;
+import com.google.gwt.gen2.table.client.TreeTable;
+import com.google.gwt.gen2.table.client.TreeTableController;
 import com.google.gwt.gen2.table.client.AbstractScrollTable.SortPolicy;
-import com.google.gwt.gen2.table.shared.ColumnFilterInfo;
-import com.google.gwt.gen2.table.shared.ColumnFilterList;
-import com.google.gwt.gen2.table.shared.ColumnSortInfo;
-import com.google.gwt.gen2.table.shared.ColumnSortList;
-import com.google.gwt.gen2.table.shared.SerializableResponse;
-import com.google.gwt.gen2.table.shared.TreeItem;
-import com.google.gwt.gen2.table.shared.TreeRequest;
-import com.google.gwt.gen2.table.shared.TreeTableItem;
+import com.google.gwt.gen2.table.shared.AbstractTreeTableItem;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.Random;
@@ -30,276 +19,123 @@ import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 public class TreeTableDemo implements EntryPoint {
-	class File implements TreeTableItem {
-		private String path, name;
-		private double size;
-		private Date lastModified;
-		private TreeTableItem parent;
-		private boolean children;
-		private int page, row;
+  static class Ancestor extends AbstractTreeTableItem {
+    private String firstName, lastName;
+    private double iq;
+    private Date dateOfBirth;
 
-		public File(String path, String name, TreeTableItem parent, boolean children, double size, Date lastModified) {
-			this.path = path;
-			this.name = name;
-			this.parent = parent;
-			this.children = children;
-			this.size = size;
-			this.lastModified = lastModified;
-		}
+    public Ancestor(String firstName, String lastName, int iq, Date dateOfBirth) {
+      super();
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.iq = iq;
+      this.dateOfBirth = dateOfBirth;
+    }
 
-		public String getId() {
-			return path;
-		}
+    public String getFirstName() {
+      return firstName;
+    }
 
-		public String getDisplayName() {
-			return name;
-		}
+    public String getLastName() {
+      return lastName;
+    }
 
-		public TreeTableItem getParent() {
-			return parent;
-		}
+    public double getIq() {
+      return iq;
+    }
 
-		public boolean hasChildren() {
-			return children;
-		}
+    public Date getDateOfBirth() {
+      return dateOfBirth;
+    }
 
-		public String getName() {
-			return name;
-		}
+    @Override
+    public String getDisplayName() {
+      return firstName+" "+lastName;
+    }
 
-		public void setName(String name) {
-			this.name = name;
-		}
+    @Override
+    public String getId() {
+      return firstName+lastName+dateOfBirth;
+    }
+  }
 
-		public double getSize() {
-			return size;
-		}
+  private static String[] firstNames = {"Miguel", "Fred", "Bob", "Emily", "John", "Ray", "Bruce", "Ray", "Larry", "Sergey", "Bill", "Steve", "Daniel" };
+  private static String[] lastNames = {"Mendez", "Sauer", "Vatwer", "Crutcher", "LaBanca", "Ryan", "Johnson", "Cromwell", "Page", "Bin", "Gates", "Jobs", "Florey" };
+  
+  public void onModuleLoad() {
+    DatePicker.injectDefaultCss();
 
-		public void setSize(double size) {
-			this.size = size;
-		}
+    // Create some dummy data
+    List<TreeItem<Ancestor>> files = new ArrayList<TreeItem<Ancestor>>();
+    for (int i = 0; i < 12; i++) {
+      TreeItem<Ancestor> ancestor = createAncestor();
+      addChildren(ancestor, Random.nextInt(3) + 1, 0);
+      files.add(ancestor);
+    }
+    // Create table definition
+    DefaultTableDefinition<Ancestor> tableDefinition = new DefaultTableDefinition<Ancestor>();
+    tableDefinition.addColumnDefinition(new TextColumnDefinition<Ancestor>(
+        "First name", true, false) {
+      public String getCellValue(Ancestor ancestor) {
+        return ancestor.getFirstName();
+      }
+    });
+    tableDefinition.addColumnDefinition(new TextColumnDefinition<Ancestor>(
+        "Last name", true, false) {
+      public String getCellValue(Ancestor ancestor) {
+        return ancestor.getLastName();
+      }
+    });
+    tableDefinition.addColumnDefinition(new NumberColumnDefinition<Ancestor>(
+        "IQ", NumberFormat.getDecimalFormat(), true, false) {
+      public Double getCellValue(Ancestor ancestor) {
+        return ancestor.getIq();
+      }
+    });
+    tableDefinition.addColumnDefinition(new DateColumnDefinition<Ancestor>(
+        "Date of birth", DateTimeFormat.getMediumDateFormat(), true, true,
+        false) {
+      public Date getCellValue(Ancestor ancestor) {
+        return ancestor.getDateOfBirth();
+      }
+    });
 
-		public void setPage(int page) {
-			this.page = page;
-		}
+    TreeTable<Ancestor> treeTable = new TreeTable<Ancestor>(tableDefinition,
+        files, true);
+    treeTable.setPageSize(12);
+    treeTable.setSortPolicy(SortPolicy.SINGLE_CELL);
+    treeTable.setWidth("100%");
+    treeTable.setHeight("100%");
+    treeTable.setResizePolicy(ScrollTable.ResizePolicy.FILL_WIDTH);
+    TreeTableController treeTableController = new TreeTableController(treeTable);
+    treeTableController.setWidth("100%");
+    treeTable.setSize("100%", "100%");
+    DockPanel dockPanel = new DockPanel();
+    dockPanel.setWidth("100%");
+    dockPanel.setHeight("300px");
+    dockPanel.add(treeTable, DockPanel.CENTER);
+    dockPanel.setCellHeight(treeTable, "100%");
+    dockPanel.setCellWidth(treeTable, "100%");
+    dockPanel.add(treeTableController, DockPanel.SOUTH);
+    RootPanel.get().add(dockPanel);
+  }
 
-		public void setRow(int row) {
-			this.row = row;
-		}
+  private void addChildren(TreeItem<Ancestor> parent, int numberOfChildren,
+      int level) {
+    for (int i = 0; i < numberOfChildren; i++) {
+      TreeItem<Ancestor> child = createAncestor();
+      parent.addChild(child);
+      if (level < 4) {
+        addChildren(child, Random.nextInt(4) + 1, level + 1);
+      }
+    }
+  }
 
-		public Date getLastModified() {
-			return lastModified;
-		}
-
-		public void setLastModified(Date lastModified) {
-			this.lastModified = lastModified;
-		}
-
-		public boolean isChildren() {
-			return children;
-		}
-
-		public void setChildren(boolean children) {
-			this.children = children;
-		}
-
-		public void setParent(File parent) {
-			this.parent = parent;
-		}
-
-		public int getPage() {
-			return page;
-		}
-
-		public int getRow() {
-			return row;
-		}
-	}
-
-	class ClientFileTableModel extends TreeTableModel<File> {
-		class TreeItemComparator implements Comparator<TreeItem<File>> {
-			private final ColumnDefinition<File, ?> columnDefinition;
-			private final boolean ascending;
-
-			public TreeItemComparator(ColumnDefinition<File, ?> columnDefinition, boolean ascending) {
-				this.columnDefinition = columnDefinition;
-				this.ascending = ascending;
-			}
-
-			public int compare(TreeItem<File> o1, TreeItem<File> o2) {
-				return ((Comparable) columnDefinition.getCellValue(o1.getTreeTableItem())).compareTo(columnDefinition.getCellValue(o2.getTreeTableItem())) * (ascending ? 1 : -1);
-			}
-		}
-
-		private List<TreeItem<File>> files = new ArrayList<TreeItem<File>>();
-		private final DefaultTableDefinition<File> tableDefinition;
-		private boolean filterTopLevel = true;
-		private boolean sortTopLevel = true;
-
-		public ClientFileTableModel(DefaultTableDefinition<File> fileTableDefinition) {
-			this.tableDefinition = fileTableDefinition;
-			// Create some dummy data
-			for (int i = 0; i < 10; i++) {
-				TreeItem<File> file = new TreeItem<File>(new File("C:/tmp/" + i, "File" + i, null, true, Random.nextInt(100000), new Date()));
-				addChildren(file, Random.nextInt(5) + 1, 0);
-				files.add(file);
-			}
-		}
-
-		private void addChildren(TreeItem<File> file, int numberOfChildren, int level) {
-			for (int i = 0; i < numberOfChildren; i++) {
-				TreeItem<File> child = new TreeItem<File>(new File(file.getTreeTableItem().getId() + "/" + i, "Level-" + level + "-Child-" + i, (File) file
-						.getTreeTableItem(), level < 5, Random.nextInt(100000), new Date()));
-				file.addChild(child);
-				if (level < 5) {
-					addChildren(child, Random.nextInt(5) + 1, level + 1);
-				}
-			}
-		}
-
-		@Override
-		public void requestTreeItems(TreeRequest request, Callback<File> callback) {
-			boolean open = request.isOpen();
-			Set<String> invertedNodes = request.getInvertedNodes();
-			int numRows = request.getNumRows();
-			int startRow = request.getStartRow();
-			int currentRow = 0;
-			ColumnSortList columnSortList = request.getColumnSortList();
-			ColumnSortInfo columnSortInfo = columnSortList.getPrimaryColumnSortInfo();
-			TreeItemComparator comparator = null;
-			if (columnSortInfo != null) {
-				List<ColumnDefinition<File, ?>> visibleColumnDefinitions = tableDefinition.getVisibleColumnDefinitions();
-				ColumnDefinition<File, ?> sortableColumnDefinition = null;
-				for (ColumnDefinition<File, ?> columnDefinition : visibleColumnDefinitions) {
-					if (columnDefinition.getColumnFilter().getColumn() == columnSortInfo.getColumn()) {
-						sortableColumnDefinition = columnDefinition;
-					}
-				}
-				comparator = new TreeItemComparator(sortableColumnDefinition, columnSortInfo.isAscending());
-				Collections.sort(files, comparator);
-			}
-			List<File> visibleFiles = new ArrayList<File>();
-			for (TreeItem<File> file : files) {
-				currentRow = addTreeItem(request, visibleFiles, file, open, invertedNodes, comparator, currentRow, startRow, numRows, 0);
-			}
-			callback.onRowsReady(request, new SerializableResponse<File>(visibleFiles, currentRow));
-		}
-
-		private int addTreeItem(TreeRequest request, List<File> visibleFiles, TreeItem<File> treeItem, boolean open, Set<String> invertedNodes, TreeItemComparator comparator, int currentRow,
-				int startRow, int numRows, int level) {
-			// Check if filters are matching...
-			ColumnFilterList columnFilterList = request.getColumnFilterList();
-			boolean filterMatching = false;
-			if (level == 0 && filterTopLevel) {
-				filterMatching = isFilterMatching(treeItem, columnFilterList);
-			} else {
-				filterMatching = true;
-			}
-			if (filterMatching) {
-				treeItem.getTreeTableItem().setRow(currentRow);
-				currentRow++;
-				if (currentRow > startRow && (currentRow - startRow) <= numRows) {
-					visibleFiles.add(treeItem.getTreeTableItem());
-				}
-			}
-			if (filterMatching || !filterTopLevel) {
-				if (treeItem.hasChildren()
-						&& (open && !invertedNodes.contains(treeItem.getTreeTableItem().getId()) || !open
-								&& invertedNodes.contains(treeItem.getTreeTableItem().getId()))) {
-					List<TreeItem<File>> children = treeItem.getChildren();
-					if ( comparator != null ) {
-						Collections.sort(children, comparator);
-					}
-					for (TreeItem<File> child : children) {
-						currentRow = addTreeItem(request, visibleFiles, child, open, invertedNodes, comparator, currentRow, startRow, numRows, level + 1);
-					}
-				}
-			}
-			return currentRow;
-		}
-
-		private boolean isFilterMatching(TreeItem<File> treeItem, ColumnFilterList columnFilterList) {
-			for (ColumnFilterInfo columnFilterInfo : columnFilterList) {
-				List<ColumnDefinition<File, ?>> visibleColumnDefinitions = tableDefinition.getVisibleColumnDefinitions();
-				for (ColumnDefinition<File, ?> columnDefinition : visibleColumnDefinitions) {
-					if (columnDefinition.getColumnFilter().getColumn() == columnFilterInfo.getColumn()) {
-						if (!columnFilterInfo.isFilterMatching(columnDefinition.getCellValue(treeItem.getTreeTableItem()))) {
-							return false;
-						}
-					}
-				}
-			}
-			return true;
-		}
-	}
-
-	public void onModuleLoad() {
-		DefaultTableDefinition<File> fileTableDefinition = new DefaultTableDefinition<File>();
-		fileTableDefinition.addColumnDefinition(new TextColumnDefinition<File>(true, false) {
-			@Override
-			public String getCellValue(File file) {
-				return file.getName();
-			}
-
-			@Override
-			public void setCellValue(File file, String cellValue) {
-				file.setName(cellValue);
-			}
-		});
-		fileTableDefinition.addColumnDefinition(new NumberColumnDefinition<File>(NumberFormat.getDecimalFormat(), true, false) {
-			@Override
-			public Double getCellValue(File file) {
-				return file.getSize();
-			}
-
-			@Override
-			public void setCellValue(File file, Double cellValue) {
-				file.setSize(cellValue);
-			}
-		});
-		fileTableDefinition.addColumnDefinition(new DateColumnDefinition<File>(DateTimeFormat.getMediumDateTimeFormat(), true, true, false) {
-			@Override
-			public Date getCellValue(File file) {
-				return file.getLastModified();
-			}
-
-			@Override
-			public void setCellValue(File file, Date cellValue) {
-				file.setLastModified(cellValue);
-			}
-		});
-		FixedWidthGrid dataTable = new FixedWidthGrid();
-		FixedWidthFlexTable headerTable = new FixedWidthFlexTable();
-		headerTable.setHTML(0, 0, "Name");
-		headerTable.setHTML(0, 1, "Size");
-		headerTable.setHTML(0, 2, "Last modified");
-		DatePicker.injectDefaultCss();
-		PagingScrollTreeTable<File> treeTable = new PagingScrollTreeTable<File>(new ClientFileTableModel(fileTableDefinition), dataTable, headerTable,
-				fileTableDefinition, false);
-		treeTable.setCellPadding(0);
-		treeTable.setCellSpacing(0);
-		treeTable.setSortPolicy(SortPolicy.SINGLE_CELL);
-		treeTable.setPageSize(25);
-		treeTable.setWidth("100%");
-		treeTable.setHeight("100%");
-		treeTable.setResizePolicy(ScrollTable.ResizePolicy.FILL_WIDTH);
-		PagingOptions pagingOptions = new PagingOptions(treeTable);
-		treeTable.setSize("100%", "100%");
-		DockPanel dockPanel = new DockPanel();
-		dockPanel.setWidth("100%");
-		dockPanel.setHeight("400px");
-		dockPanel.add(treeTable, DockPanel.CENTER);
-		dockPanel.setCellHeight(treeTable, "100%");
-		dockPanel.setCellWidth(treeTable, "100%");
-		dockPanel.add(pagingOptions, DockPanel.SOUTH);
-		RootPanel.get().add(dockPanel);
-	}
+  private TreeItem<Ancestor> createAncestor() {
+    return new TreeItem(new Ancestor(firstNames[Random.nextInt(firstNames.length-1)], lastNames[Random.nextInt(lastNames.length-1)], Random.nextInt(50)+80, new Date((long)Random.nextInt(1000000)*500000L)));
+  }
 }
