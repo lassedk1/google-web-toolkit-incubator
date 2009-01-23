29d3086563bfc3353223eb3be8802600dfe2da6a
ecc <ecc@google.com>
2009-01-23 18:59:41 +0000

    Fixing checkstyle errors

--- src-demo/com/google/gwt/gen2/demo/fasttree/client/FastTreeDemo.java
+++ src-demo/com/google/gwt/gen2/demo/fasttree/client/FastTreeDemo.java
@@ -16,20 +16,11 @@
 package com.google.gwt.gen2.demo.fasttree.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.logical.shared.CloseEvent;
-import com.google.gwt.event.logical.shared.CloseHandler;
-import com.google.gwt.event.logical.shared.OpenEvent;
-import com.google.gwt.event.logical.shared.OpenHandler;
-import com.google.gwt.event.logical.shared.SelectionEvent;
-import com.google.gwt.event.logical.shared.SelectionHandler;
-import com.google.gwt.gen2.commonevent.shared.BeforeCloseEvent;
-import com.google.gwt.gen2.commonevent.shared.BeforeCloseHandler;
 import com.google.gwt.gen2.commonevent.shared.BeforeOpenEvent;
 import com.google.gwt.gen2.commonevent.shared.BeforeOpenHandler;
 import com.google.gwt.gen2.complexpanel.client.FastTree;
 import com.google.gwt.gen2.complexpanel.client.FastTreeItem;
 import com.google.gwt.gen2.complexpanel.client.HasFastTreeItems;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -38,13 +29,17 @@ import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
+/**
+ * Demo for the new fast tree.
+ * 
+ */
 public class FastTreeDemo implements EntryPoint {
 
   public StackPanel createDemo() {
 
     // inject default styles
     FastTree.injectDefaultCss();
-    
+
     StackPanel p = new StackPanel();
 
     p.add(basicTree(), "Basic tree");
@@ -53,7 +48,7 @@ public class FastTreeDemo implements EntryPoint {
     p.add(crazyTree(), "Crazy tree");
     return p;
   }
-  
+
   /**
    * This is the entry point method.
    */
@@ -81,7 +76,7 @@ public class FastTreeDemo implements EntryPoint {
     widgetBranch.addItem("A ListBox parent").addItem(lb);
     return t;
   }
-  
+
   /**
    * Creates a lazy tree.
    */
@@ -94,93 +89,40 @@ public class FastTreeDemo implements EntryPoint {
           for (int i = 0; i < 50; i++) {
             lazyCreateChild(event.getTarget(), i, 50 + (i * 10));
           }
-        }        
-      }      
-    });    
+        }
+      }
+    });
     return t;
   }
-  
+
   private void lazyCreateChild(final HasFastTreeItems parent, final int index,
       final int children) {
 
-    final FastTreeItem item = new FastTreeItem("child" + index + " (" + children
-        + " children)");
-    
+    final FastTreeItem item = new FastTreeItem("child" + index + " ("
+        + children + " children)");
+
     item.becomeInteriorNode();
     parent.addItem(item);
   }
-  
-  protected Widget verboseTree() {
-    FastTree tree = new FastTree();
-    verboseTreeItem(tree, 10);
-    tree.addOpenHandler(new OpenHandler<FastTreeItem>() {
 
-      public void onOpen(OpenEvent<FastTreeItem> event) {
-        Window.alert("Opened " + event.getTarget().getHTML());
-      }      
-    });
-    tree.addCloseHandler(new CloseHandler<FastTreeItem>() {
+  private void lazyCreateChild(final HasFastTreeItems parent, final int index,
+      final int children) {
 
-        public void onClose(CloseEvent<FastTreeItem> event) {
-          Window.alert("Close " + event.getTarget().getHTML());          
-        }
-      });
-    tree.addSelectionHandler(new SelectionHandler<FastTreeItem>() {
+    final FastTreeItem item = new FastTreeItem("child" + index + " ("
+        + children + " children)");
 
-        public void onSelection(SelectionEvent<FastTreeItem> event) {
-          Window.alert("You selected " + event.getSelectedItem().getHTML());
-        }
-      });
-    tree.addBeforeOpenHandler(new BeforeOpenHandler<FastTreeItem>() {
-
-      public void onBeforeOpen(BeforeOpenEvent<FastTreeItem> event) {
-        if (Window.confirm("Would you like to change the name of the item before opening it?")) {
-          event.getTarget().setHTML("Name changed before open.");
-        }
-      }        
-    });
-    tree.addBeforeCloseHandler(new BeforeCloseHandler<FastTreeItem>() {
-
-        public void onBeforeClose(BeforeCloseEvent<FastTreeItem> event) {
-          if (Window.confirm("Would you like to change the name of the item before closing it?")) {
-            event.getTarget().setHTML("Name changed before close.");
-          }
-        }        
-      });
-    return tree;
+    item.becomeInteriorNode();
+    parent.addItem(item);
   }
-  
+
   private void verboseTreeItem(HasFastTreeItems parent, int children) {
     for (int i = 0; i < children; i++) {
       final int index = i;
-      
+
       FastTreeItem item = new FastTreeItem();
-      item.setText("item " + index);  
+      item.setText("item " + index);
       parent.addItem(item);
       verboseTreeItem(item, children - (i + 1));
     }
-  }
-  
-  private Widget crazyTree() {
-    FastTree tree = new FastTree();
-    FastTreeItem a = tree.addItem("I am root");
-    a.addItem("I am root's child");
-    FastTreeItem aXb = a.addItem("Root to become leaf.");
-    aXb.addItem("a grand child");
-    for (int i = 0; i < 10; i++) {
-      aXb.addItem("grand child " + i);
-    }
-    tree.addOpenHandler(new OpenHandler<FastTreeItem>() {
-
-      public void onOpen(OpenEvent<FastTreeItem> event) {
-        if (!event.getTarget().isLeafNode()) {
-          if (Window.confirm("Would you like me to become a leaf?")) {
-            event.getTarget().becomeLeaf();
-          }
-        }
-      }        
-    });
-   
-    return tree;
   }
 }
