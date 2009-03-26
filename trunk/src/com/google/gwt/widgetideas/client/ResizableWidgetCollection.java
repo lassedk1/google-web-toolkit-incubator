5786ea9ef14b12c0d236724a16597b99782c011e
jlabanca@google.com <jlabanca@google.com@6497d715-d039-0410-8d7d-ef101778ce2c>
2009-03-26 21:25:07 +0000

    ResizableWidgetCollection now provides a way for widgets to update their size, preventing an extra rendering loop.
    Patch by: jlabanca

--- trunk/src/com/google/gwt/widgetideas/client/ResizableWidgetCollection.java
+++ trunk/src/com/google/gwt/widgetideas/client/ResizableWidgetCollection.java
@@ -294,4 +294,25 @@ public class ResizableWidgetCollection implements WindowResizeListener,
       resizeCheckTimer.cancel();
     }
   }
+
+  /**
+   * Inform the {@link ResizableWidgetCollection} that the size of a widget has
+   * changed and already been redrawn. This will prevent the widget from being
+   * redrawn on the next loop.
+   * 
+   * @param widget the widget's size that changed
+   */
+  public void updateWidgetSize(ResizableWidget widget) {
+    if (!widget.isAttached()) {
+      return;
+    }
+
+    ResizableWidgetInfo info = widgets.get(widget);
+    if (info != null) {
+      int curWidth = widget.getElement().getPropertyInt("clientWidth");
+      int curHeight = widget.getElement().getPropertyInt("clientHeight");
+      info.setClientSize(curWidth, curHeight);
+    }
+  }
+
 }
