f4405b66fe014640b8172424ee0c2b22bbc2198c
jlabanca@google.com <jlabanca@google.com@6497d715-d039-0410-8d7d-ef101778ce2c>
2009-04-15 18:42:12 +0000

    Fixed a focus issue in FastTree where clicking on a large TreeItem would scroll it into view on mouse down, preventing a full click from occuring.
    Patch by: jlabanca
    Review by: rjrjr

--- trunk/src/com/google/gwt/gen2/complexpanel/client/FastTree.java
+++ trunk/src/com/google/gwt/gen2/complexpanel/client/FastTree.java
@@ -803,7 +803,7 @@ public class FastTree extends Panel implements HasClickHandlers,
         disableSelection(target);
         return;
       }
-      onSelection(item, true, !shouldTreeDelegateFocusToElement(target));
+      onSelection(item, true, false);
     }
     return;
   }
