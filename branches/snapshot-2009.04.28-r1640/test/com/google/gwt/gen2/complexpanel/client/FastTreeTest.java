b5c7a671a962711bba67e4916ec7cc5d88667fe5
spoon@google.com <spoon@google.com@6497d715-d039-0410-8d7d-ef101778ce2c>
2009-04-29 21:33:49 +0000

    Creates snapshot branch snapshot-2009.04.28-r1640 using command:
    svn copy -r1640 https://google-web-toolkit-incubator.googlecode.com/svn/trunk https://google-web-toolkit-incubator.googlecode.com/svn/branches/snapshot-2009.04.28-r1640

---
+++ branches/snapshot-2009.04.28-r1640/test/com/google/gwt/gen2/complexpanel/client/FastTreeTest.java
@@ -0,0 +1,321 @@
+/*
+ * Copyright 2009 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.gen2.complexpanel.client;
+
+import com.google.gwt.event.logical.shared.BeforeSelectionEvent;
+import com.google.gwt.event.logical.shared.BeforeSelectionHandler;
+import com.google.gwt.gen2.base.client.Gen2TestBase;
+import com.google.gwt.gen2.commonwidget.client.HasWidgetsTester;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Test for fast tree.
+ */
+public class FastTreeTest extends Gen2TestBase {
+
+  static class Adder implements HasWidgetsTester.WidgetAdder {
+    public void addChild(HasWidgets container, Widget child) {
+      ((FastTree) container).addItem(child);
+    }
+  }
+
+  // TODO(nwolf/ecc): Figure out why this isn't working, currently unknown.
+  public void disabledTestAttachDetachOrder() {
+    HasWidgetsTester.testAll(new FastTree(), new Adder());
+  }
+
+  public void testClear() {
+    FastTree t = new FastTree();
+    // Adding widget to end of FastTree, widgets still have their parents set
+    // correctly.
+    FastTreeItem a = new FastTreeItem("a");
+    FastTreeItem b = new FastTreeItem("b");
+    FastTreeItem c = new FastTreeItem("c");
+    FastTreeItem d = new FastTreeItem();
+    FastTreeItem e = new FastTreeItem();
+    Label dLabel = new Label("d");
+    Label eLabel = new Label("e");
+    d.setWidget(dLabel);
+    d.addItem(e);
+    c.addItem(d);
+    b.addItem(c);
+    a.addItem(b);
+    t.addItem(a);
+    t.addItem("hello");
+    t.addItem(eLabel);
+    t.clear();
+  }
+
+  public void testInsertSameItemRepeatedly() {
+    FastTree t = new FastTree();
+    FastTreeItem ti = new FastTreeItem();
+    FastTreeItem wti = new FastTreeItem();
+    wti.setWidget(new Label("label"));
+    for (int i = 0; i < 10; i++) {
+      t.addItem(ti);
+      t.addItem(wti);
+    }
+    assertEquals(2, t.getItemCount());
+    assertEquals(ti, t.getItem(0));
+    assertEquals(wti, t.getItem(1));
+  }
+
+  public void testRootAdd() {
+    FastTree t = new FastTree();
+    Label l = new Label("hello");
+    t.add(l);
+    assertEquals(t, l.getParent());
+  }
+
+  public void testSwap() {
+    FastTree t = new FastTree();
+
+    // Start with text.
+    FastTreeItem item = t.addItem("hello");
+    String inner = DOM.getInnerHTML(item.getContentElement());
+    assertTrue(inner.indexOf("hello") >= 0);
+    t.addItem(item);
+    Widget goodbyeWidget = new Label("goodbye");
+    item.setWidget(goodbyeWidget);
+    String innerWidget = DOM.getInnerHTML(item.getContentElement());
+    assertFalse(innerWidget.indexOf("hello") >= 0);
+
+    // Start with widget.
+    Widget hello = new Label("hello");
+    FastTreeItem widgetItem = t.addItem(hello);
+    assertTrue(DOM.getInnerHTML(widgetItem.getContentElement()).indexOf("hello") >= 0);
+    widgetItem.setText("goodbye");
+    assertFalse(DOM.getInnerHTML(widgetItem.getContentElement()).indexOf(
+        "hello") >= 0);
+    assertTrue(DOM.getInnerHTML(widgetItem.getContentElement()).indexOf(
+        "goodbye") >= 0);
+    assertNull(hello.getParent());
+
+    // Move widget.
+    widgetItem.addItem(item);
+    assertEquals(t, goodbyeWidget.getParent());
+    assertEquals(goodbyeWidget, item.getWidget());
+
+    // Set back to text.
+    item.setText("aloha");
+    assertEquals("aloha", DOM.getInnerHTML(item.getContentElement()));
+    assertNull(goodbyeWidget.getParent());
+    assertNull(item.getWidget());
+  }
+
+  public void testFastTree() {
+    // Simple widget
+    FastTree t = new FastTree();
+    Label l = new Label("simple widget");
+    FastTreeItem simple = new FastTreeItem(l);
+    t.addItem(simple);
+
+    // Adding test and widget
+    FastTreeItem item = new FastTreeItem();
+    t.addItem(item);
+    item.setWidget(new Label("now widget"));
+    Element elem = item.getContentElement();
+    assertEquals(1, DOM.getChildCount(elem));
+
+    // Add widget to existing FastTree.
+    Label l2 = new Label("next widget");
+    simple.setWidget(l2);
+    assertEquals(t, l2.getParent());
+
+    // Remove a FastTree item, make sure widget is removed from FastTree, but
+    // not FastTree
+    // item.
+    simple.remove();
+    assertEquals(l2, simple.getWidget());
+    assertNull(l2.getParent());
+
+    // Adding widget to end of FastTree, widgets still have their parents set
+    // correctly.
+    FastTreeItem a = new FastTreeItem("a");
+    FastTreeItem b = new FastTreeItem("b");
+    FastTreeItem c = new FastTreeItem("c");
+    FastTreeItem d = new FastTreeItem();
+    FastTreeItem e = new FastTreeItem();
+    Label dLabel = new Label("d");
+    Label eLabel = new Label("e");
+    d.setWidget(dLabel);
+    d.addItem(e);
+    c.addItem(d);
+    b.addItem(c);
+    a.addItem(b);
+    t.addItem(a);
+    assertEquals(t, dLabel.getParent());
+    e.setWidget(eLabel);
+    assertEquals(t, eLabel.getParent());
+
+    // FastTree inside of FastTree.
+    FastTree childFastTree = new FastTree();
+    t.addItem(new FastTreeItem(childFastTree));
+
+    // Swap FastTreeItems to new FastTree.
+    childFastTree.addItem(c);
+    assertEquals(childFastTree, dLabel.getParent());
+    assertEquals(childFastTree, eLabel.getParent());
+
+    // Make sure remove clears.
+    d.remove();
+    assertNull(dLabel.getParent());
+    assertNull(eLabel.getParent());
+    assertFalse(childFastTree.getChildWidgets().containsKey(eLabel.getParent()));
+  }
+  
+  public void testBeforeSelectRespectsCancel() {
+    FastTree tree = createBeforeSelectTestableTree(true);
+    
+    // Get first branch
+    FastTreeItem item0 = tree.getItem(0);
+    tree.setSelectedItem(item0);
+    assertEquals("a", item0.getText());
+    assertFalse(item0.isSelected());
+    
+    // Get second branch
+    FastTreeItem item1 = tree.getItem(1);
+    tree.setSelectedItem(item1);
+    assertEquals("b", item1.getText());
+    assertTrue(item1.isSelected());
+    
+    // Now that the second branch is open
+    // Try to select the first child
+    FastTreeItem item2 = item1.getChild(0);
+    tree.setSelectedItem(item2);
+    assertEquals("b-a", item2.getText());
+    assertFalse(item2.isSelected());
+    assertTrue(item1.isSelected());
+    
+    FastTreeItem item3 = item1.getChild(1);
+    tree.setSelectedItem(item3);
+    assertEquals("b-b", item3.getText());
+    assertTrue(item3.isSelected());
+    assertFalse(item1.isSelected());
+  }
+  
+  public void testBeforeSelectNotFiredWhenEventsAreOff() {
+    FastTree tree = createBeforeSelectTestableTree(true);
+    
+    // Get first branch
+    FastTreeItem item0 = tree.getItem(0);
+    tree.setSelectedItem(item0, false);
+    assertEquals("a", item0.getText());
+    assertTrue(item0.isSelected());
+    
+    // Get second branch
+    FastTreeItem item1 = tree.getItem(1);
+    tree.setSelectedItem(item1, false);
+    assertEquals("b", item1.getText());
+    assertTrue(item1.isSelected());
+    
+    // Now that the second branch is open
+    // Try to select the first child
+    FastTreeItem item2 = item1.getChild(0);
+    tree.setSelectedItem(item2, false);
+    assertEquals("b-a", item2.getText());
+    assertTrue(item2.isSelected());
+    assertFalse(item1.isSelected());
+    
+    FastTreeItem item3 = item1.getChild(1);
+    tree.setSelectedItem(item3, false);
+    assertEquals("b-b", item3.getText());
+    assertTrue(item3.isSelected());
+    assertFalse(item1.isSelected());
+  }
+  
+  public void testBeforeSelectNotFiredWhenUndefined() {
+    FastTree tree = createBeforeSelectTestableTree(false);
+    
+    // Get first branch
+    FastTreeItem item0 = tree.getItem(0);    
+    assertNotNull(tree.beforeSelected(item0));
+    tree.setSelectedItem(item0);
+    assertEquals("a", item0.getText());
+    assertTrue(item0.isSelected());
+        
+    // Get second branch
+    FastTreeItem item1 = tree.getItem(1);
+    tree.setSelectedItem(item1);
+    assertEquals("b", item1.getText());
+    assertTrue(item1.isSelected());
+    
+    // Now that the second branch is open
+    // Try to select the first child
+    FastTreeItem item2 = item1.getChild(0);
+    tree.setSelectedItem(item2);
+    assertEquals("b-a", item2.getText());
+    assertTrue(item2.isSelected());
+    assertFalse(item1.isSelected());
+    
+    FastTreeItem item3 = item1.getChild(1);
+    tree.setSelectedItem(item3);
+    assertEquals("b-b", item3.getText());
+    assertTrue(item3.isSelected());
+    assertFalse(item1.isSelected());
+  }
+  
+  public void testFindDeepestOpenChild() {
+    FastTree tree = createBeforeSelectTestableTree(false);
+    FastTreeItem item = tree.getItem(0);
+    item.setState(true);
+    tree.setSelectedItem(item);
+    assertTrue(item.isOpen());
+    assertEquals(2, item.getChildCount());
+    
+    // no try something funky
+    item.removeItems();
+    
+    try {
+      assertNotNull(tree.findDeepestOpenChild(item));
+    } catch (IndexOutOfBoundsException iobe) {
+      fail("Out of bound exception thrown!");
+    }
+  }
+
+  /**
+   * helper method for testing beforeSelect event.
+   * @param hasBeforeSelectionHandler TODO
+   */
+  private FastTree createBeforeSelectTestableTree(
+      boolean hasBeforeSelectionHandler) {
+    final FastTree tree = new FastTree();
+    FastTreeItem firstBranch = tree.addItem("a");
+    firstBranch.addItem("a-a");
+    firstBranch.addItem("a-b");
+    FastTreeItem secondBranch = tree.addItem("b");
+    secondBranch.addItem("b-a");
+    secondBranch.addItem("b-b");
+    
+    if (hasBeforeSelectionHandler) {
+      tree.addBeforeSelectionHandler(
+          new BeforeSelectionHandler<FastTreeItem>() {
+            public void onBeforeSelection(
+                BeforeSelectionEvent<FastTreeItem> event) {
+                if (event.getItem().getText().contains("a")) {
+                  event.cancel();
+                }
+            }
+      });
+    }
+    
+    return tree;
+  }
+}
---
+++ branches/snapshot-2009.04.28-r1640/test/com/google/gwt/gen2/table/client/ColumnResizerTest.java
@@ -0,0 +1,239 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.gen2.table.client;
+
+import com.google.gwt.gen2.base.client.Gen2TestBase;
+import com.google.gwt.gen2.table.client.ColumnResizer.ColumnWidthInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tests methods in the {@link ColumnResizer} class.
+ */
+public class ColumnResizerTest extends Gen2TestBase {
+  /**
+   * Test the column width info.
+   */
+  public void testCurrentColumnWidthInfo() {
+    // getPercentageDifference
+    {
+      ColumnWidthInfo info = new ColumnWidthInfo(100, 300, 200, 150);
+
+      // getters
+      assertEquals(150, info.getCurrentWidth());
+      assertEquals(100, info.getMinimumWidth());
+      assertEquals(300, info.getMaximumWidth());
+      assertEquals(200, info.getPreferredWidth());
+
+      // percentage difference
+      info.setNewWidth(150);
+      assertEquals(-0.25, info.getPercentageDifference(), 0.001);
+
+      // required width
+      info.setRequiredWidth(40);
+      assertEquals(40, info.getRequiredWidth());
+
+      // new width
+      info.setNewWidth(190);
+      assertEquals(190, info.getNewWidth());
+    }
+  }
+
+  /**
+   * Verify that if the current sizes are outside of the min/max range, the new
+   * sizes will be forced into the range.
+   */
+  public void testCurrentWidthOutOfRange() {
+    // Current width below minimum width
+    {
+      // Create some column info
+      List<ColumnWidthInfo> columns = new ArrayList<ColumnWidthInfo>();
+      columns.add(new ColumnWidthInfo(200, 300, 250, 0));
+      columns.add(new ColumnWidthInfo(200, 300, 50, 50));
+      columns.add(new ColumnWidthInfo(200, 300, 350, 199));
+
+      // Distribute some width
+      ColumnResizer resizer = new ColumnResizer();
+      int remaining = resizer.distributeWidth(columns, 0);
+      assertEquals(-351, remaining);
+
+      // Check outputs
+      assertEquals(200, columns.get(0).getNewWidth());
+      assertEquals(200, columns.get(1).getNewWidth());
+      assertEquals(200, columns.get(2).getNewWidth());
+    }
+
+    // Current width above maximum width
+    {
+      // Create some column info
+      List<ColumnWidthInfo> columns = new ArrayList<ColumnWidthInfo>();
+      columns.add(new ColumnWidthInfo(200, 300, 250, 350));
+      columns.add(new ColumnWidthInfo(200, 300, 50, 1000));
+      columns.add(new ColumnWidthInfo(200, 300, 350, 301));
+
+      // Distribute some width
+      ColumnResizer resizer = new ColumnResizer();
+      int remaining = resizer.distributeWidth(columns, 0);
+      assertEquals(751, remaining);
+
+      // Check outputs
+      assertEquals(300, columns.get(0).getNewWidth());
+      assertEquals(300, columns.get(1).getNewWidth());
+      assertEquals(300, columns.get(2).getNewWidth());
+    }
+  }
+
+  /**
+   * Verify that we can distribute a negative width.
+   */
+  public void testDistributeNegativeWidth() {
+    // Create some column info
+    ColumnResizer resizer = new ColumnResizer();
+    List<ColumnWidthInfo> columns = new ArrayList<ColumnWidthInfo>();
+    columns.add(new ColumnWidthInfo(100, 300, 200, 150));
+    columns.add(new ColumnWidthInfo(0, 100, 50, 50));
+    columns.add(new ColumnWidthInfo(0, 500, 300, 151));
+
+    // Only the first column gets any width, goes to -10%
+    {
+      assertEquals(0, resizer.distributeWidth(columns, -5));
+      assertEquals(150, columns.get(0).getNewWidth());
+      assertEquals(45, columns.get(1).getNewWidth());
+      assertEquals(151, columns.get(2).getNewWidth());
+    }
+
+    // First two columns get width, go to -30%
+    {
+      assertEquals(0, resizer.distributeWidth(columns, -25));
+      assertEquals(140, columns.get(0).getNewWidth(), 1);
+      assertEquals(35, columns.get(1).getNewWidth(), 1);
+      assertEquals(151, columns.get(2).getNewWidth(), 1);
+    }
+
+    // All columns get width, go to -50%
+    {
+      assertEquals(0, resizer.distributeWidth(columns, -76));
+      assertEquals(100, columns.get(0).getNewWidth());
+      assertEquals(25, columns.get(1).getNewWidth());
+      assertEquals(150, columns.get(2).getNewWidth());
+    }
+
+    // Too much width to distribute
+    {
+      assertEquals(-49, resizer.distributeWidth(columns, -300));
+      assertEquals(100, columns.get(0).getNewWidth());
+      assertEquals(0, columns.get(1).getNewWidth());
+      assertEquals(0, columns.get(2).getNewWidth());
+    }
+
+    // Difference results in columns out of proportion by 1
+    {
+      assertEquals(0, resizer.distributeWidth(columns, -8));
+      assertEquals(149, columns.get(0).getNewWidth(), 1);
+      assertEquals(43, columns.get(1).getNewWidth(), 1);
+      assertEquals(151, columns.get(2).getNewWidth(), 1);
+    }
+
+    // Difference results in columns out of proportion by 2
+    {
+      assertEquals(0, resizer.distributeWidth(columns, -78));
+      assertEquals(99, columns.get(0).getNewWidth(), 1);
+      assertEquals(24, columns.get(1).getNewWidth(), 1);
+      assertEquals(150, columns.get(2).getNewWidth(), 1);
+    }
+  }
+
+  /**
+   * Verify that we can distribute a positive width.
+   */
+  public void testDistributePositiveWidth() {
+    // Create some column info
+    ColumnResizer resizer = new ColumnResizer();
+    List<ColumnWidthInfo> columns = new ArrayList<ColumnWidthInfo>();
+    columns.add(new ColumnWidthInfo(100, 300, 200, 150));
+    columns.add(new ColumnWidthInfo(0, 100, 50, 50));
+    columns.add(new ColumnWidthInfo(0, 500, 300, 100));
+
+    // Only the first column gets any width, goes to -50%
+    {
+      assertEquals(0, resizer.distributeWidth(columns, 50));
+      assertEquals(150, columns.get(0).getNewWidth());
+      assertEquals(50, columns.get(1).getNewWidth());
+      assertEquals(150, columns.get(2).getNewWidth());
+    }
+
+    // First two columns get width, go to -10%
+    {
+      assertEquals(0, resizer.distributeWidth(columns, 200));
+      assertEquals(180, columns.get(0).getNewWidth());
+      assertEquals(50, columns.get(1).getNewWidth());
+      assertEquals(270, columns.get(2).getNewWidth());
+    }
+
+    // All columns get width, go to +10%
+    {
+      assertEquals(0, resizer.distributeWidth(columns, 305));
+      assertEquals(220, columns.get(0).getNewWidth());
+      assertEquals(55, columns.get(1).getNewWidth());
+      assertEquals(330, columns.get(2).getNewWidth());
+    }
+
+    // Too much width to distribute
+    {
+      assertEquals(400, resizer.distributeWidth(columns, 1000));
+      assertEquals(300, columns.get(0).getNewWidth());
+      assertEquals(100, columns.get(1).getNewWidth());
+      assertEquals(500, columns.get(2).getNewWidth());
+    }
+
+    // Difference results in columns out of proportion by 1
+    {
+      assertEquals(0, resizer.distributeWidth(columns, 201));
+      assertEquals(180, columns.get(0).getNewWidth());
+      assertEquals(50, columns.get(1).getNewWidth());
+      assertEquals(271, columns.get(2).getNewWidth());
+    }
+
+    // Difference results in columns out of proportion by 2
+    {
+      assertEquals(0, resizer.distributeWidth(columns, 251));
+      assertEquals(200, columns.get(0).getNewWidth());
+      assertEquals(50, columns.get(1).getNewWidth());
+      assertEquals(301, columns.get(2).getNewWidth());
+    }
+  }
+
+  /**
+   * Verify that we can distribute a zero width.
+   */
+  public void testDistributeZeroWidth() {
+    // Create some column info
+    List<ColumnWidthInfo> columns = new ArrayList<ColumnWidthInfo>();
+    columns.add(new ColumnWidthInfo(100, 300, 200, 150));
+    columns.add(new ColumnWidthInfo(0, 100, 50, 50));
+    columns.add(new ColumnWidthInfo(0, 500, 300, 200));
+
+    // Distribute some width
+    ColumnResizer resizer = new ColumnResizer();
+    resizer.distributeWidth(columns, 0);
+
+    // Check outputs
+    assertEquals(150, columns.get(0).getNewWidth());
+    assertEquals(50, columns.get(1).getNewWidth());
+    assertEquals(200, columns.get(2).getNewWidth());
+  }
+}
