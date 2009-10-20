51ad1772fcf263600755355867337c2fc03141d6
jlabanca@google.com <jlabanca@google.com@6497d715-d039-0410-8d7d-ef101778ce2c>
2009-10-20 20:04:45 +0000

    Modifying DefaultCellRenderer so it calls cellView#setText() instead of cellView#setHtml() by default to make it consistent with other widgets, which require users to opt-in to html.
    Patch by: jlabanca

--- trunk/src/com/google/gwt/gen2/table/client/DefaultCellRenderer.java
+++ trunk/src/com/google/gwt/gen2/table/client/DefaultCellRenderer.java
@@ -20,23 +20,48 @@ import com.google.gwt.user.client.ui.Widget;
 
 /**
  * The default {@link CellRenderer} used by the {@link AbstractColumnDefinition}
- * when the user does not specify one.
+ * when the user does not specify one. By default, {@link Widget}s are rendered
+ * as {@link Widget}s, and all other values are rendered as text.
+ * 
  * 
  * @param <RowType> the type of the row value
  * @param <ColType> the data type of the column
  */
 public class DefaultCellRenderer<RowType, ColType> implements
     CellRenderer<RowType, ColType> {
+
+  /**
+   * If true, text will be rendered as html.
+   */
+  private boolean asHtml;
+
+  /**
+   * Construct a new {@link DefaultCellRenderer}.
+   */
+  public DefaultCellRenderer() {
+  }
+
+  /**
+   * Construct a new {@link DefaultCellRenderer}.
+   * 
+   * @param asHtml if true, non-widget cell contents will be rendered as html
+   */
+  public DefaultCellRenderer(boolean asHtml) {
+    this.asHtml = asHtml;
+  }
+
   public void renderRowValue(RowType rowValue,
       ColumnDefinition<RowType, ColType> columnDef,
       AbstractCellView<RowType> view) {
     Object cellValue = columnDef.getCellValue(rowValue);
     if (cellValue == null) {
-      view.setHTML("");
+      view.setText("");
     } else if (cellValue instanceof Widget) {
       view.setWidget((Widget) cellValue);
-    } else {
+    } else if (asHtml) {
       view.setHTML(cellValue.toString());
+    } else {
+      view.setText(cellValue.toString());
     }
   }
 }
